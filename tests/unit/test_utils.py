b0f0677a246e64ebfaa9d61c7dc458c1a8ed354d
Lamboll <rlamboll@ic.ac.uk>
2019-09-23 18:25:06 +0100

    Working quantile finders for rolling and fixed windows, with tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -17,13 +17,14 @@ def test_rolling_window_find_quantiles():
     ys = np.array([0, 1, 0.5])
     desired_quantiles = [0.4, 0.5, 0.6]
     quantiles = utils.rolling_window_find_quantiles(xs, ys, desired_quantiles, 9, 1)
-    assert all(np.round(quantiles.iloc[0].astype(float), 3) == np.round([1.0/3, 0.5, 2.0/3], 3))
-    assert (np.round(quantiles.iloc[-1, 0], 5) == np.round(0.5*(0.4-0.125)/(0.5-0.125), 5))
+    assert all(quantiles.iloc[0] == desired_quantiles)
+    assert all(quantiles.iloc[1] == desired_quantiles)
 
     xs = np.array([0, 0, 1, 1])
     ys = np.array([0, 0, 1, 1])
     quantiles = utils.rolling_window_find_quantiles(xs, ys, desired_quantiles, 9, 1)
     assert all(quantiles.iloc[[0, 1], 0] == 0)
+    assert all(quantiles.iloc[[-1, -2], -1] == 1)
 
 
 test_aggregate_and_find_quantiles()
