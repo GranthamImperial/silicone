e156a44acfaf2b3a5b27392a03750fbd54b53d8a
Lamboll <rlamboll@ic.ac.uk>
2019-09-23 17:53:43 +0100

    Working function for midpoint-based rolling window quantiles calculations

--- src/silicone/utils.py
+++ src/silicone/utils.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from skmisc.loess import loess
 
 # A function to return the quantiles of the data that fit into different boxes.
 # Overshoot: the distance beyond the real limits of x to include in our plots, assumed 1
@@ -29,3 +28,31 @@ def aggregate_and_find_quantiles(xs, ys, quantiles=[0.2, 0.33, 0.5, 0.66, 0.8], 
         else:
             quantmatrix.iloc[ind, ] = np.quantile(ys[ins], quantiles)
     return quantmatrix
+
+
+# Divides the x-axis into nboxes of equal length and weights data by how close they are to the center
+# of these boxes. Then finds quantiles of this weighted data.
+# The x and y co-ordinates, xs and ys, should be numpy arrays of the same length.
+# decay_length gives the distance over which the weighting of the values falls to 1/2, given by equation
+# w = 1/(1+(distance/decay_length)^2). This defaults to the interbox distance if no argument is given.
+# Note that this uses a different definition of quantiles to the np.quantile function, where the uppermost and
+# lowermost points are 0/1. Here we identify halfway the quantile as equalling the value halfway through a weight
+# cumulative sum, or linear interpolation between these values if there is none.
+def rolling_window_find_quantiles(xs, ys, quantiles=[0.2, 0.33, 0.5, 0.66, 0.8], nboxes=10, decay_length=None):
+    assert xs.size == ys.size
+    step = (max(xs) - min(xs)) / (nboxes+1)
+    if decay_length is None:
+        decay_length = step
+    sort_order = np.argsort(ys)
+    ys = ys[sort_order]
+    xs = xs[sort_order]
+    box_centers = np.arange(min(xs), max(xs)+step, step)
+    quantmatrix = pd.DataFrame(index=box_centers, columns=quantiles)
+    for ind in range(box_centers.size):
+        weights = 1.0/(1.0+((xs-box_centers[ind])/decay_length)**2)
+        # We want to calculate the weights at the midpoint of step corresponding to the y-value.
+        cumsum_weights = np.cumsum(weights)-0.5*weights
+        cumsum_weights /= sum(weights)
+        quantmatrix.iloc[ind, ] = np.interp(quantiles, cumsum_weights, ys)
+    return quantmatrix
+
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,4 +1,4 @@
-from src.silicone.utils import aggregate_and_find_quantiles
+import src.silicone.utils as utils
 import numpy as np
 import pandas as pd
 
@@ -6,7 +6,25 @@ def test_aggregate_and_find_quantiles():
     xs = np.array([0, 0, 1])
     ys = np.array([0, 1, 1])
     desired_quantiles = [0.2, 0.5, 0.8]
-    quantiles = aggregate_and_find_quantiles(xs, ys, desired_quantiles)
+    quantiles = utils.aggregate_and_find_quantiles(xs, ys, desired_quantiles)
     assert all(quantiles.iloc[0] == desired_quantiles)
     assert all(pd.isna(quantiles.iloc[1]))
     assert quantiles.iloc[-1, -1] == 1
+
+def test_rolling_window_find_quantiles():
+
+    xs = np.array([0, 0, 1])
+    ys = np.array([0, 1, 0.5])
+    desired_quantiles = [0.4, 0.5, 0.6]
+    quantiles = utils.rolling_window_find_quantiles(xs, ys, desired_quantiles, 9, 1)
+    assert all(np.round(quantiles.iloc[0].astype(float), 3) == np.round([1.0/3, 0.5, 2.0/3], 3))
+    assert (np.round(quantiles.iloc[-1, 0], 5) == np.round(0.5*(0.4-0.125)/(0.5-0.125), 5))
+
+    xs = np.array([0, 0, 1, 1])
+    ys = np.array([0, 0, 1, 1])
+    quantiles = utils.rolling_window_find_quantiles(xs, ys, desired_quantiles, 9, 1)
+    assert all(quantiles.iloc[[0, 1], 0] == 0)
+
+
+test_aggregate_and_find_quantiles()
+test_rolling_window_find_quantiles()
