18f39063e1e80b1c0d96e2276d8f5977992530b3
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-10-05 15:37:24 +1000

    Add some base clases, might remove later

---
+++ src/silicone/db_crunchers/rolling_window.py
@@ -0,0 +1,8 @@
+from .base import _DBCruncherBase
+
+
+class DBCruncherRollingWindow(_DBCruncherBase):
+    """Rolling window database cruncher"""
+    def __init__(self, arg):
+        super(DBCruncherRollingWindow, self).__init__()
+        self.arg = arg
