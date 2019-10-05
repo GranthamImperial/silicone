11b3452ee52c8ad1a994e4f865580a137c81a719
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-10-05 16:39:54 +1000

    Add failing tests

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+import os
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import pytest
+from pyam import IamDataFrame
+
+TEST_DB = pd.DataFrame(
+    [
+        [
+            "model_a",
+            "scen_a",
+            "World",
+            "Emissions|HFC|C5F12",
+            "kt C5F12/yr",
+            np.nan,
+            3.14,
+        ]
+    ],
+    columns=["model", "scenario", "region", "variable", "unit", 2010, 2015],
+)
+
+TEST_YEARS = [2010, 2015]
+TEST_DTS = [datetime(2010, 6, 17), datetime(2015, 7, 21)]
+
+
+@pytest.fixture(
+    scope="function",
+    params=[
+        TEST_YEARS,
+        TEST_DTS,
+        ["2010-06-17", "2015-07-21"],
+        ["2010-06-17 00:00:00", "2015-07-21 12:00:00"],
+    ],
+)
+def test_db(request):
+    tdf = TEST_DB.copy()
+    tdf = tdf.rename({2010: request.param[0], 2015: request.param[1]}, axis="columns")
+    df = IamDataFrame(data=tdf)
+    yield df
+
+
+@pytest.fixture(scope="function")
+def test_downscale_df(request):
+    return IamDataFrame(request.cls.tdownscale_df)
