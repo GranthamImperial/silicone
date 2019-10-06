521dd01924e7d6d13c72cf2688ce4cd4ba7489cd
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-10-06 15:41:23 +1100

    Increase test coverage

--- tests/integration/crunchers/base.py
+++ tests/integration/crunchers/base.py
@@ -1,10 +1,18 @@
 import datetime as dt
+import re
 from abc import ABCMeta, abstractmethod
 
+import pytest
 from pyam import IamDataFrame
 
 
 class _DataBaseCruncherTester(metaclass=ABCMeta):
+    # crunching class to test
+    tclass = None
+
+    # dataframe to use when testing downscaling (i.e. dataframe containing lead gases)
+    tdownscale_df = None
+
     def _join_iamdfs_time_wrangle(self, base, other):
         return base.append(self._adjust_time_style_to_match(other, base))
 
@@ -28,3 +36,22 @@ class _DataBaseCruncherTester(metaclass=ABCMeta):
     def test_relationship_usage(self):
         """Test that derived relationship gives expected results when used"""
         pass
+
+    def test_relationship_usage_wrong_time_col(self, test_db, test_downscale_df):
+        tcruncher = self.tclass(test_db)
+
+        filler = tcruncher.derive_relationship(
+            "Emissions|HFC|C5F12", ["Emissions|HFC|C2F6"]
+        )
+
+        if test_db.time_col == "year":
+            test_downscale_df = test_downscale_df.timeseries()
+            test_downscale_df.columns = test_downscale_df.columns.map(lambda x: dt.datetime(x, 1, 1))
+            test_downscale_df = IamDataFrame(test_downscale_df)
+
+        error_msg = re.escape(
+            "`in_iamdf` time column must be the same as the time column used "
+            "to generate this filler function (`{}`)".format(test_db.time_col)
+        )
+        with pytest.raises(ValueError, match=error_msg):
+            filler(test_downscale_df, interpolate=True)
