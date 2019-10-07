750af2682128841b3ac1a1578ce4c452d2ad6f44
Lamboll <rlamboll@ic.ac.uk>
2019-10-07 14:49:29 +0100

    Minor redefintion of quantiles function under a precise equality condition.

--- src/silicone/utils.py
+++ src/silicone/utils.py
@@ -54,7 +54,9 @@ def rolling_window_find_quantiles(xs, ys, quantiles, nboxes=10, decay_length_fac
     ys = ys[sort_order]
     xs = xs[sort_order]
     if max(xs) == min(xs):
+        # We must prevent singularity behaviour if all the points are at the same x value.
         box_centers = np.array([xs[0]])
+        decay_length = 1
     else:
         # We want to include the max x point, but not any point above it.
         # The 0.99 factor prevents rounding error inclusion.
@@ -66,6 +68,5 @@ def rolling_window_find_quantiles(xs, ys, quantiles, nboxes=10, decay_length_fac
         # We want to calculate the weights at the midpoint of step corresponding to the y-value.
         cumsum_weights = np.cumsum(weights)
         for i_quantile in range(quantiles.__len__()):
-            quantmatrix.iloc[ind, i_quantile] = (min(ys[cumsum_weights > quantiles[i_quantile]]) +
-                                                 min(ys[cumsum_weights >= quantiles[i_quantile]])) / 2
+            quantmatrix.iloc[ind, i_quantile] = min(ys[cumsum_weights >= quantiles[i_quantile]])
     return quantmatrix
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -21,16 +21,20 @@ def test_rolling_window_find_quantiles():
     ys = np.array([0, 1, 0, 1])
     desired_quantiles = [0.4, 0.5, 0.6]
     quantiles = utils.rolling_window_find_quantiles(xs, ys, desired_quantiles, 9, 2*9)
-    assert all(quantiles.iloc[0] == [0, 0.5, 1])
-    assert all(quantiles.iloc[1] == [0, 0.5, 1])
+    assert all(quantiles.iloc[0] == [0, 0, 1])
+    assert all(quantiles.iloc[1] == [0, 0, 1])
 
     xs = np.array([0, 0, 1, 1])
     ys = np.array([0, 0, 1, 1])
     quantiles = utils.rolling_window_find_quantiles(xs, ys, desired_quantiles, 9, 2*9)
     assert all(quantiles.iloc[0, :] == 0)
     assert all(quantiles.iloc[-1, :] == 1)
-    assert all(quantiles.iloc[5, :] == [0, 0.5, 1])
+    assert all(quantiles.iloc[5, :] == [0, 0, 1])
 
     desired_quantiles = [0, 0.5, 1]
     quantiles = utils.rolling_window_find_quantiles(np.array([1]), np.array([1]), desired_quantiles, 9, 2 * 9)
+    assert all(quantiles.iloc[0, :] == [1, 1, 1])
+
+    desired_quantiles = [0, 0.5, 1]
+    quantiles = utils.rolling_window_find_quantiles(np.array([1, 1]), np.array([1, 1]), desired_quantiles, 9, 2 * 9)
     assert all(quantiles.iloc[0, :] == [1, 1, 1])
