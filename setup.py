3610c640f1ccde835c7de998036d4879c7399c56
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-10-16 09:12:06 +1100

    Update setup py properly

--- setup.py
+++ setup.py
@@ -26,8 +26,9 @@ CLASSIFIERS = [
     #   5 - Production/Stable
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
-    # "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
 ]
 REQUIREMENTS_INSTALL = [
