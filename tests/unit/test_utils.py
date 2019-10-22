77b0d54b54ed7b7209cc8293bdce527892c9af9c
Lamboll <rlamboll@ic.ac.uk>
2019-10-22 17:38:57 +0100

    Added a test of the new find closest function

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,8 +1,24 @@
-from silicone.utils import add_example
-
+from silicone.utils import add_example, select_closest
+import pandas as pd
+import pytest
 
 def test_addition():
     expected = 4
     result = add_example(1, 3)
 
     assert expected == result
+
+
+def test_select_closest():
+    target = pd.Series([1, 2, 3])
+    bad_answer = pd.DataFrame([1, 1])
+    possible_answers = pd.DataFrame([[1, 1, 1], [1, 2, 3.5], [1, 2, 3.5], [1, 2, 4]])
+
+    with pytest.raises(ValueError, match="Target array is multidimensional"):
+        select_closest(bad_answer, bad_answer)
+
+    with pytest.raises(ValueError, match="Target array does not match the size of the searchable arrays"):
+        select_closest(bad_answer, target)
+
+    index_of_answer = select_closest(possible_answers, target)
+    assert index_of_answer == 1
