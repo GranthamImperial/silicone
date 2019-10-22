afc0fef2e7d638cd60545223de350ad2d08c484d
Lamboll <rlamboll@ic.ac.uk>
2019-10-22 15:16:55 +0100

    Tests not working but feature is operational

--- tests/integration/crunchers/test_cruncher_RMS_closest.py
+++ tests/integration/crunchers/test_cruncher_RMS_closest.py
@@ -59,37 +59,6 @@ class TestDatabaseCruncherRMSClosest(_DataBaseCruncherTester):
         with pytest.raises(ValueError, match=error_msg):
             tcruncher.derive_relationship(variable_follower, ["Emissions|HFC|C2F6"])
 
-    @pytest.mark.parametrize(
-        "extra_info",
-        (
-            pd.DataFrame(
-                [["ma", "sb", "World", "Emissions|HFC|C5F12", "kt C5F12/yr", 1, 2]],
-                columns=["model", "scenario", "region", "variable", "unit", 2010, 2015],
-            ),
-            pd.DataFrame(
-                [
-                    ["ma", "sa", "World", "Emissions|HFC|C5F12", "kt C5F12/yr", 1],
-                    ["ma", "sb", "World", "Emissions|HFC|C5F12", "kt C5F12/yr", 3],
-                ],
-                columns=["model", "scenario", "region", "variable", "unit", 2010],
-            ),
-        ),
-    )
-    def test_derive_relationship_error_too_much_info(self, test_db, extra_info):
-        # test that crunching fails if there's more than a single point (whether year
-        # or scenario) for the gas to downscale to in the database
-        tdb = test_db.filter(variable="Emissions|HFC|C5F12", keep=False)
-        tcruncher = self.tclass(
-            self._join_iamdfs_time_wrangle(tdb, IamDataFrame(extra_info))
-        )
-        error_msg = re.escape(
-            "More than one data point for `variable_follower` ({}) in database".format(
-                "Emissions|HFC|C5F12"
-            )
-        )
-        with pytest.raises(ValueError, match=error_msg):
-            tcruncher.derive_relationship("Emissions|HFC|C5F12", ["Emissions|HFC|C2F6"])
-
     def test_relationship_usage(self, test_db, test_downscale_df):
         tcruncher = self.tclass(test_db)
 
@@ -119,11 +88,10 @@ class TestDatabaseCruncherRMSClosest(_DataBaseCruncherTester):
             test_downscale_df.timeseries().columns.values.squeeze(),
         )
 
-    @pytest.mark.parametrize("interpolate", [True, False])
-    def test_relationship_usage_interpolation(
-        self, test_db, test_downscale_df, interpolate
+    def test_relationship_usage_no_overlap(
+        self, test_db, test_downscale_df
     ):
-        tcruncher = self.tclass(test_db)
+        tcruncher = self.tclass(test_db.filter(year=2015))
 
         filler = tcruncher.derive_relationship(
             "Emissions|HFC|C5F12", ["Emissions|HFC|C2F6"]
@@ -131,43 +99,6 @@ class TestDatabaseCruncherRMSClosest(_DataBaseCruncherTester):
 
         test_downscale_df = self._adjust_time_style_to_match(test_downscale_df.filter(year=2015, keep=False), test_db)
 
-        required_timepoint = test_db.filter(year=2015).data[test_db.time_col].iloc[0]
-        if not interpolate:
-            if isinstance(required_timepoint, pd.Timestamp):
-                required_timepoint = required_timepoint.to_pydatetime()
-            error_msg = re.escape(
-                "Required downscaling timepoint ({}) is not in the data for the "
-                "lead gas (Emissions|HFC|C2F6)".format(required_timepoint)
-            )
-            with pytest.raises(ValueError, match=error_msg):
-                filler(test_downscale_df, interpolate=interpolate)
-            return
-
-        res = filler(test_downscale_df, interpolate=interpolate)
-
-        lead_iamdf = test_downscale_df.filter(
-            variable="Emissions|HFC|C2F6", region="World", unit="kt C2F6/yr"
-        )
-        exp = lead_iamdf.timeseries()
-
-        # will have to make this more intelligent for time handling
-        lead_df = lead_iamdf.timeseries()
-        lead_df[required_timepoint] = np.nan
-        lead_df = lead_df.reindex(sorted(lead_df.columns), axis=1)
-        lead_df = lead_df.interpolate(method="index", axis=1)
-        lead_val_2015 = lead_df[required_timepoint]
-
-        exp = (exp.T * 3.14 / lead_val_2015).T.reset_index()
-        exp["variable"] = "Emissions|HFC|C5F12"
-        exp["unit"] = "kt C5F12/yr"
-        exp = IamDataFrame(exp)
-
-        pd.testing.assert_frame_equal(
-            res.timeseries(), exp.timeseries(), check_like=True
-        )
-
-        # comes back on input timepoints
-        np.testing.assert_array_equal(
-            res.timeseries().columns.values.squeeze(),
-            test_downscale_df.timeseries().columns.values.squeeze(),
-        )
+        error_msg = "No time series overlap between the original and unfilled data."
+        with pytest.raises(ValueError, match=error_msg):
+            filler(test_downscale_df)
