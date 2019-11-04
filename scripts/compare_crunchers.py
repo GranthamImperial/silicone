f1313be534417124876b46ba71af92ca5f6fe043
Lamboll <rlamboll@ic.ac.uk>
2019-11-04 17:19:55 +0000

    Refined comparison and added a plotter function

--- scripts/compare_crunchers.py
+++ scripts/compare_crunchers.py
@@ -9,11 +9,9 @@ import matplotlib.pyplot as plt
 This script measures how accurate the different crunchers are at recreating known data
 
 """
-# ___________________________Input options____________________________________
+# __________________________________Input options_______________________________________
 # Where is the file stored for data used to fill in the sheet?
-input_data = (
-    "../Input/SSP_CMIP6_201811.csv"
-)
+input_data = "../Input/SSP_CMIP6_201811.csv"
 # A list of all crunchers to investigate, here a reference to the actual cruncher
 crunchers_list = [silicone.database_crunchers.DatabaseCruncherQuantileRollingWindows]
 # This list must agree with the above list, but is the name of the crunchers
@@ -21,36 +19,72 @@ crunchers_name_list = ["QuantileRolling"]
 # Leader is a single data class for the moment, but presented as a list.
 leaders = ["CMIP6 Emissions|CO2"]
 # This is the model/scenario combination to compare.
-to_compare_filter = {'model': 'AIM/CGE', 'scenario': 'SSP3-70 (Baseline)'}
+to_compare_filter = {"model": "IMAGE", "scenario": "SSP1-19"}
 # Place to save the infilled data as a csvn
-save_file = "../Output/CruncherComparison.csv"
-# ____________________________end options____________________________________
+save_file = "../Output/CruncherResults/CruncherComparison.csv"
+# Do we want to save plots? If not, leave as None, else the location to save them.
+save_plots = "../Output/CruncherResults/"
+# __________________________________end options_________________________________________
 
 assert len(crunchers_list) == len(crunchers_name_list)
 
-db = pyam.IamDataFrame(input_data).filter(region="World")
-real_results = db.filter(**to_compare_filter)
-db.filter(**to_compare_filter, keep=False, inplace=True)
-vars_to_crunch = [
-    req for req in db.variables() if req not in leaders
-]
+db_all = pyam.IamDataFrame(input_data).filter(region="World")
+real_results = db_all.filter(**to_compare_filter)
+db = db_all.filter(**to_compare_filter, keep=False)
+vars_to_crunch = [req for req in db.filter(level=1).variables() if req not in leaders]
 # Initialise the object that holds the results
 results_db = pd.DataFrame(index=vars_to_crunch, columns=crunchers_name_list)
 
 for cruncher_ind in range(len(crunchers_list)):
     cruncher_instance = crunchers_list[cruncher_ind](db)
     for var_inst in vars_to_crunch:
+        var_units = db.filter(variable=var_inst).variables(True)["unit"]
+        assert (
+            var_units.size == 1
+        ), "Multiple units involved, this spoils the calculation"
         filler = cruncher_instance.derive_relationship(var_inst, leaders)
         interpolated = filler(real_results)
-        originals = real_results.filter(variable=var_inst).data.set_index("year")["value"]
+        originals = real_results.filter(variable=var_inst).data.set_index("year")[
+            "value"
+        ]
         # Currently I am normalising by the mean absolute value
         mean_abs = statistics.mean(abs(originals))
         interp_values = interpolated.data.set_index("year")["value"]
-        assert originals.size == interp_values.size, \
-            "Wrong number of values returned"
-        assert interpolated['year'].size == interpolated['year'].unique().size, \
-            "The wrong number of years have returned values"
-        results_db[crunchers_name_list[cruncher_ind]][var_inst] = statistics.mean((interp_values-originals)**2)**0.5/mean_abs
+        assert originals.size == interp_values.size, "Wrong number of values returned"
+        assert (
+            interpolated["year"].size == interpolated["year"].unique().size
+        ), "The wrong number of years have returned values"
+        results_db[crunchers_name_list[cruncher_ind]][var_inst] = (
+            statistics.mean((interp_values - originals) ** 2) ** 0.5 / mean_abs
+        )
+        if save_plots:
+            plt.close()
+            ax = plt.subplot(111)
+            plt.scatter(x=originals.index, y=originals, label="True values", alpha=0.8)
+            plt.scatter(
+                x=interp_values.index,
+                y=interp_values,
+                label="Model values",
+                marker="s",
+                alpha=0.5,
+            )
+            plt.xlabel("Year")
+            plt.ylabel("Emissions of {} ({})".format(var_inst, var_units[0]))
+            to_plot = db_all.filter(variable=var_inst)
+            plt.scatter(
+                to_plot["year"],
+                to_plot["value"],
+                label="Other values",
+                alpha=0.5,
+                marker="v",
+                s=8,
+            )
+            plt.legend()
+            plt.savefig(
+                save_plots
+                + "{}_{}.png".format(
+                    crunchers_name_list[cruncher_ind], var_inst.split("|")[-1]
+                )
+            )
 
-
-
+    results_db.to_csv(save_file)
