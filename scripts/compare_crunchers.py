37e47e87a62e7efb6d68a22a9a1e4b4d6329b1a5
Lamboll <rlamboll@ic.ac.uk>
2019-11-05 10:16:50 +0000

    Reformat file for whitespace issues

--- scripts/compare_crunchers.py
+++ scripts/compare_crunchers.py
@@ -13,16 +13,19 @@ This script measures how accurate the different crunchers are at recreating know
 # Where is the file stored for data used to fill in the sheet?
 input_data = "../Input/SSP_CMIP6_201811.csv"
 # A list of all crunchers to investigate, here a reference to the actual cruncher
-crunchers_list = [silicone.database_crunchers.DatabaseCruncherQuantileRollingWindows]
+crunchers_list = [
+    silicone.database_crunchers.DatabaseCruncherQuantileRollingWindows,
+    silicone.database_crunchers.DatabaseCruncherRMSClosest,
+]
 # This list must agree with the above list, but is the name of the crunchers
-crunchers_name_list = ["QuantileRolling"]
+crunchers_name_list = ["QuantileRolling", "RMSClosest"]
 # Leader is a single data class for the moment, but presented as a list.
 leaders = ["CMIP6 Emissions|CO2"]
 # Place to save the infilled data as a csvn
 save_file = "../Output/CruncherResults/CruncherComparison.csv"
 # Do we want to save plots? If not, leave as None, else the location to save them.
 # Note that these are not filter-dependent and so only the results of the last filter will persist
-save_plots = None # "../Output/CruncherResults/"
+save_plots = None  # "../Output/CruncherResults/"
 # Do we want to run this for all possible filters? If so, choose none,
 # otherwise specify the filter here
 to_compare_filter = None  # e.g. ("GCAM4","SSP4-34")
@@ -35,10 +38,15 @@ db_all = pyam.IamDataFrame(input_data).filter(region="World")
 if to_compare_filter:
     all_possible_filters = to_compare_filter
 else:
-    all_possible_filters = db_all.data[["model", "scenario"]] \
-        .groupby(["model", "scenario"]).size().index.values
-vars_to_crunch = [req for req in db_all.filter(level=1).variables()
-                  if req not in leaders]
+    all_possible_filters = (
+        db_all.data[["model", "scenario"]]
+        .groupby(["model", "scenario"])
+        .size()
+        .index.values
+    )
+vars_to_crunch = [
+    req for req in db_all.filter(level=1).variables() if req not in leaders
+]
 overall_results = pd.DataFrame(index=vars_to_crunch, columns=crunchers_name_list)
 for one_filter in all_possible_filters:
     combo_filter = {"model": one_filter[0], "scenario": one_filter[1]}
@@ -63,7 +71,9 @@ for one_filter in all_possible_filters:
             # Currently I am normalising by the mean absolute value
             mean_abs = statistics.mean(abs(originals))
             interp_values = interpolated.data.set_index("year")["value"]
-            assert originals.size == interp_values.size, "Wrong number of values returned"
+            assert (
+                originals.size == interp_values.size
+            ), "Wrong number of values returned"
             assert (
                 interpolated["year"].size == interpolated["year"].unique().size
             ), "The wrong number of years have returned values"
@@ -73,7 +83,9 @@ for one_filter in all_possible_filters:
             if save_plots:
                 plt.close()
                 ax = plt.subplot(111)
-                plt.scatter(x=originals.index, y=originals, label="True values", alpha=0.8)
+                plt.scatter(
+                    x=originals.index, y=originals, label="True values", alpha=0.8
+                )
                 plt.scatter(
                     x=interp_values.index,
                     y=interp_values,
@@ -99,5 +111,7 @@ for one_filter in all_possible_filters:
                         crunchers_name_list[cruncher_ind], var_inst.split("|")[-1]
                     )
                 )
-    overall_results = overall_results.fillna(0)+results_db.fillna(0)/all_possible_filters.size
+    overall_results = (
+        overall_results.fillna(0) + results_db.fillna(0) / all_possible_filters.size
+    )
 overall_results.to_csv(save_file)
