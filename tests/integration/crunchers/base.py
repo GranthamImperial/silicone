a332de525ba839f033c921633d2150690e545d0c
Lamboll <rlamboll@ic.ac.uk>
2020-02-17 18:41:13 +0000

    Added another feature to reduce code length

--- tests/integration/crunchers/base.py
+++ tests/integration/crunchers/base.py
@@ -4,6 +4,7 @@ from abc import ABCMeta, abstractmethod
 
 import pytest
 from pyam import IamDataFrame
+from silicone.utils import _adjust_time_style_to_match
 
 
 class _DataBaseCruncherTester(metaclass=ABCMeta):
@@ -20,20 +21,7 @@ class _DataBaseCruncherTester(metaclass=ABCMeta):
         return base.append(self._adjust_time_style_to_match(other, base))
 
     def _adjust_time_style_to_match(self, in_df, target_df):
-        if in_df.time_col != target_df.time_col:
-            in_df = in_df.timeseries()
-            if target_df.time_col == "time":
-                target_df_year_map = {v.year: v for v in target_df.timeseries().columns}
-                in_df.columns = in_df.columns.map(
-                    lambda x: target_df_year_map[x]
-                    if x in target_df_year_map
-                    else dt.datetime(x, 1, 1)
-                )
-            else:
-                in_df.columns = in_df.columns.map(lambda x: x.year)
-            return IamDataFrame(in_df)
-
-        return in_df
+        return _adjust_time_style_to_match(in_df, target_df)
 
     @abstractmethod
     def test_derive_relationship(self, test_db):
