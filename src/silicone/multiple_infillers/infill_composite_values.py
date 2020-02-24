0bccfce7ef565a3214950e6421686a81f3662bbe
Lamboll <rlamboll@ic.ac.uk>
2020-02-24 15:04:15 +0000

    Added a function to infill composite values based on the disaggregated values

---
+++ src/silicone/multiple_infillers/infill_composite_values.py
@@ -0,0 +1,60 @@
+import logging
+from silicone.utils import _construct_consistent_values
+
+logger = logging.getLogger(__name__)
+
+
+def infill_composite_values(df, composite_dic=None):
+    """
+    Constructs a series of aggregate variables, calculated as the sums of variables
+    that have been reported. This will overwrite the original values if they exist.
+
+    Parameters
+    ----------
+    df : :obj:`pyam.IamDataFrame`
+        Input data from which to construct consistent values. This is assumed to be
+        fully infilled. This will not be thoroughly checked.
+
+    composite_dic : dict {str: list[str]}
+        Key: The variable names of the composite. Value: The variable names of the
+        constituents, which may include wildcards ('*'). Defaults to a list of
+        PFC, HFC, F-Gases, CO2 and Kyoto gases.
+    """
+    if not composite_dic:
+        composite_dic = {
+            "Emissions|PFC": ["Emissions|PFC|*"],
+            "Emissions|HFC": ["Emissions|HFC|*"],
+            "Emissions|F-Gases": [
+                "Emissions|PFC",
+                "Emissions|HFC",
+                "Emissions|SF6",
+            ],
+            "Emissions|CO2": ["Emissions|CO2|*"],
+            "Emissions|Kyoto Gases (AR5-GWP100)": [
+                "Emissions|CO2",
+                "Emissions|CH4",
+                "Emissions|N2O",
+                "Emissions|F-Gases",
+            ],
+        }
+    # Do a simple test to ensure we have any relevant data. This does not prevent
+    # problems from missing subsets of data.
+    to_delete = []
+    # Disable core logging to prevent warnings about empty filters
+    logging.getLogger("pyam.core").setLevel(logging.CRITICAL)
+    for composite in composite_dic.keys():
+        if df.filter(variable=composite_dic.get(composite)).data.empty:
+            logger.warning("No data found for {}".format(composite_dic.get(composite)))
+            to_delete.append(composite)
+    logging.getLogger("pyam.core").setLevel(logging.WARNING)
+    [composite_dic.pop(x) for x in to_delete]
+    df.filter(variable=composite_dic.keys(), keep=False, inplace=True)
+    for composite in composite_dic.keys():
+        composite_df = _construct_consistent_values(
+            composite, composite_dic.get(composite), df
+        )
+        try:
+            to_return.append(composite_df)
+        except NameError:
+            to_return = composite_df
+    return to_return
