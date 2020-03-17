1d6288755b0961db8da8e931c9e52d80ae6b283d
Lamboll <rlamboll@ic.ac.uk>
2020-03-17 00:46:17 +0000

    Added a case counter for each variable to the correlations calc

--- scripts/run_calc_correlations_between_gases.py
+++ scripts/run_calc_correlations_between_gases.py
@@ -1,6 +1,6 @@
 import silicone.stats
 import pyam
-
+import pandas as pd
 
 # This script shows how to use the investigatory script, _plot_emission_correlations_
 # quantile_rolling_windows. It saves CSV files indicating statistics of correlation
@@ -10,6 +10,8 @@ import pyam
 years = range(2020, 2101, 10)
 # Folder to save results:
 output_dir = "../Output/Correlations"
+# name of counts of variables
+var_count_file = "variable_counts.csv"
 # Location of data:
 input_data = "./sr15_scenarios_more_regions.csv"
 # If there is any prefix on the names of variables (before 'Emissions') it goes here
@@ -29,4 +31,14 @@ if prefix:
         lambda x: x.replace(prefix, "")
     )
 
+#This part counts the number of instances of a variable
+maps = pd.Series(index=sr15_data.variables(False), dtype=int)
+for var in sr15_data.variables(False):
+    var_db = sr15_data.filter(variable=var)
+    maps[var] = len(var_db.timeseries())
+var_save_loc = output_dir + "/" + var_count_file
+maps.to_csv(var_save_loc)
+print("Counted the number of each variable and saved to ".format(var_save_loc))
+
+# This part runs the correlations analysis
 silicone.stats.calc_all_emissions_correlations(sr15_data, years, output_dir)
