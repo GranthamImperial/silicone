30d6f535d15518a07744c09a6a06ca22146d8257
Lamboll <rlamboll@ic.ac.uk>
2020-03-18 16:22:58 +0000

    Rearranging to lead towards multiprocessing

--- scripts/compare_crunchers.py
+++ scripts/compare_crunchers.py
@@ -2,7 +2,9 @@ import pyam
 import silicone.database_crunchers as dc
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from silicone.plotting import _plot_reconstruct_value_with_cruncher
+from multiprocessing import Pool
+import os
 
 """
 This script measures how accurate the different crunchers are at recreating known data.
@@ -40,20 +42,90 @@ save_file = "../Output/CruncherResults/CruncherComparisonLead_{}.csv".format(
 # Do we want to save plots? If not, leave as None, else the location to save them.
 # Note that these are not filter-dependent and so only the results of the last filter
 # will persist
-save_plots = None  #  "../Output/CruncherResults/plots/"
+save_plots = None #  "../Output/CruncherResults/plots/"
 # Do we want to run this for all possible filters? If so, choose none,
 # otherwise specify the filter here as a list of tuples
-to_compare_filter = [
+to_compare_filter = None
+"""[
     ("MESSAGE-GLOBIOM 1.0", "SSP3-45"),
     ("WITCH-GLOBIOM 4.2", "ADVANCE_2020_Med2C"),
     ("AIM/CGE 2.0", "SSP1-19"),
     ("AIM/CGE 2.1", "TERL_15D_NoTransportPolicy"),
 ]
+"""
 years = range(2020, 2101, 10)
 # __________________________________end options_________________________________________
 
 assert len(crunchers_list) == len(crunchers_name_list)
 assert len(options_list) == len(crunchers_name_list)
+
+def _recalc_and_compare_results(
+        one_filter, db_all, vars_to_crunch, crunchers_name_list, crunchers_list,
+        save_plots
+):
+    combo_filter = {"model": one_filter[0], "scenario": one_filter[1]}
+    input_to_fill = db_all.filter(**combo_filter)
+    results_db = pd.DataFrame(index=vars_to_crunch, columns=crunchers_name_list)
+    if leaders not in input_to_fill.variables(False).values:
+        print("No data for {} in model {}, scen {}".format(
+            leaders, one_filter[0], one_filter[1]
+        ))
+        return results_db
+    # Remove all items that overlap directly with this
+    db_filter = db_all.filter(**combo_filter, keep=False)
+    for cruncher_ind in range(len(crunchers_list)):
+        for var_inst in vars_to_crunch:
+            originals = input_to_fill.filter(variable=var_inst).data.set_index("year")[
+                "value"
+            ]
+            if originals.empty:
+                print("No data available for {}".format(var_inst))
+                continue
+            valid_scenarios = db_filter.filter(variable=var_inst).scenarios()
+            db = db_filter.filter(scenario=valid_scenarios)
+            # Initialise the object that holds the results
+            cruncher_instance = crunchers_list[cruncher_ind](db)
+            var_units = db.filter(variable=var_inst).variables(True)["unit"]
+            assert (
+                    var_units.size == 1
+            ), "Multiple units involved, this spoils the calculation"
+            filler = cruncher_instance.derive_relationship(
+                var_inst, leaders, **options_list[cruncher_ind]
+            )
+            interpolated = filler(input_to_fill)
+            interp_values = interpolated.data.set_index("year")["value"]
+            if originals.size != interp_values.size:
+                print("Wrong number of values from cruncher {}: {}, not {}".format(
+                    crunchers_name_list[cruncher_ind],
+                    interp_values.size, originals.size
+                ))
+                continue
+            assert (
+                    interpolated["year"].size == interpolated["year"].unique().size
+            ), "The wrong number of years have returned values"
+            # Set up normalisation
+            norm_factor = pd.Series(index=interp_values.index, dtype=float)
+            for year in norm_factor.index:
+                norm_factor[year] = max(
+                    db_all.filter(year=year, variable=var_inst).data["value"]
+                ) - min(
+                    db_all.filter(year=year, variable=var_inst).data["value"]
+                )
+            # Calculate the RMS difference, Normalised by the spread of values
+            results_db[crunchers_name_list[cruncher_ind]][var_inst] = (
+                np.nanmean(((interp_values - originals)[norm_factor > 0] /
+                    norm_factor[norm_factor > 0]) ** 2)
+            ) ** 0.5
+            if save_plots:
+                _plot_reconstruct_value_with_cruncher(
+                    var_inst, var_units, interp_values, originals,
+                    crunchers_name_list[cruncher_ind], save_plots, db_all
+                )
+        print("Completed cruncher {}".format(
+            crunchers_name_list[cruncher_ind])
+        )
+        return results_db
+
 
 db_all = pyam.IamDataFrame(input_data).filter(region="World", year=years)
 db_all.filter(variable="Emissions|Kyoto Gases*", keep=False, inplace=True)
@@ -72,89 +144,13 @@ vars_to_crunch = [
 ]
 overall_results = pd.DataFrame(index=vars_to_crunch, columns=crunchers_name_list)
 results_count = pd.DataFrame(index=vars_to_crunch, columns=crunchers_name_list, data=0)
+
+# pool = Pool(os.cpu_count() - 1)
 for one_filter in all_possible_filters:
-    combo_filter = {"model": one_filter[0], "scenario": one_filter[1]}
-    input_to_fill = db_all.filter(**combo_filter)
-    # Remove all items that overlap directly with this
-    db_filter = db_all.filter(**combo_filter, keep=False)
-    results_db = pd.DataFrame(index=vars_to_crunch, columns=crunchers_name_list)
-    for cruncher_ind in range(len(crunchers_list)):
-        for var_inst in vars_to_crunch:
-            originals = input_to_fill.filter(variable=var_inst).data.set_index("year")[
-                "value"
-            ]
-            if originals.empty:
-                print("No data available for {}".format(var_inst))
-                continue
-            valid_scenarios = db_filter.filter(variable=var_inst).scenarios()
-            db = db_filter.filter(scenario=valid_scenarios)
-            # Initialise the object that holds the results
-            cruncher_instance = crunchers_list[cruncher_ind](db)
-            var_units = db.filter(variable=var_inst).variables(True)["unit"]
-            assert (
-                var_units.size == 1
-            ), "Multiple units involved, this spoils the calculation"
-            filler = cruncher_instance.derive_relationship(
-                var_inst, leaders, **options_list[cruncher_ind]
-            )
-            interpolated = filler(input_to_fill)
-            interp_values = interpolated.data.set_index("year")["value"]
-            if originals.size != interp_values.size:
-                print("Wrong number of values from cruncher {}: {}, not {}".format(
-                    crunchers_name_list[cruncher_ind],
-                    interp_values.size, originals.size
-                ))
-                continue
-            assert (
-                interpolated["year"].size == interpolated["year"].unique().size
-            ), "The wrong number of years have returned values"
-            # Set up normalisation
-            norm_factor = pd.Series(index=interp_values.index, dtype=float)
-            for year in norm_factor.index:
-                norm_factor[year] = max(
-                    db_all.filter(year=year, variable=var_inst).data["value"]
-                ) - min(
-                    db_all.filter(year=year, variable=var_inst).data["value"]
-                )
-            # Calculate the RMS difference, Normalised by the spread of values
-            results_db[crunchers_name_list[cruncher_ind]][var_inst] = (
-                np.nanmean(((interp_values - originals)[norm_factor > 0] /
-                            norm_factor[norm_factor > 0]) ** 2)
-            ) ** 0.5
-            if not np.isfinite(results_db[crunchers_name_list[cruncher_ind]][var_inst]):
-                print("year: ".format(year))
-            if save_plots:
-                plt.close()
-                ax = plt.subplot(111)
-                plt.scatter(
-                    x=originals.index, y=originals, label="True values", alpha=0.8
-                )
-                plt.scatter(
-                    x=interp_values.index,
-                    y=interp_values,
-                    label="Model values",
-                    marker="s",
-                    alpha=0.5,
-                )
-                plt.xlabel("Year")
-                plt.ylabel("Emissions of {} ({})".format(var_inst, var_units[0]))
-                to_plot = db_all.filter(variable=var_inst)
-                plt.scatter(
-                    to_plot["year"],
-                    to_plot["value"],
-                    label="Other values",
-                    alpha=0.5,
-                    marker="v",
-                    s=8,
-                )
-                plt.legend()
-                plt.savefig(
-                    save_plots
-                    + "{}_{}.png".format(
-                        crunchers_name_list[cruncher_ind], var_inst.split("|")[-1]
-                    )
-                )
-        print("Completed cruncher {}".format(crunchers_name_list[cruncher_ind]))
+    results_db = _recalc_and_compare_results(
+        one_filter, db_all, vars_to_crunch, crunchers_name_list, crunchers_list,
+        save_plots
+    )
     results_count = results_count + results_db.notnull()
     overall_results = (
         overall_results.fillna(0) + results_db.fillna(0)
