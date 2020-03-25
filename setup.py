df3ce63fc780f9dc7d432247211032ed011c9c0b
Lamboll <rlamboll@ic.ac.uk>
2020-03-25 00:36:43 +0000

    Re-insert nb-black, removed by merge conflict

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ REQUIREMENTS_DOCS = [
 ]
 REQUIREMENTS_DEPLOY = ["setuptools>=38.6.0", "twine>=1.11.0", "wheel>=0.31.0"]
 REQUIREMENTS_DEV = (
-    ["black", "bandit", "coverage", "flake8", "isort", "mypy", "pydocstyle", "pylint"]
+    ["black", "black-nb", "bandit", "coverage", "flake8", "isort", "mypy", "nbdime", "pydocstyle", "pylint"]
     + REQUIREMENTS_NOTEBOOKS
     + REQUIREMENTS_TESTS
     + REQUIREMENTS_DOCS
