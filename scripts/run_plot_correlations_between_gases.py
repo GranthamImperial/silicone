61af92871351994d557212a156986be2cccc57ab
Lamboll <rlamboll@ic.ac.uk>
2020-03-25 14:29:45 +0000

    Implemented the second half of the changes from the investigation package

--- scripts/run_plot_correlations_between_gases.py
+++ scripts/run_plot_correlations_between_gases.py
@@ -2,37 +2,48 @@ from download_sr15_emissions import download_or_load_sr15
 
 import silicone.plotting
 
-# This script shows how to use the investigatory script, _plot_emission_correlations_quantile_rolling_windows.
-# It saves plots of the relationship between carbon dioxide and other emissions to the '../Output' folder,
-# along with CSV files indicating statistics of correlation coefficients.
+# This script shows how to use the investigatory script, _plot_emission_correlations_
+# quantile_rolling_windows. It saves plots of the relationship between carbon dioxide
+# and other emissions to the '../Output' folder
 # ________________________________________________________
-
+# We must indicate the main gas of interest
+x_gases = [
+            "Emissions|PFC",
+            "Emissions|CH4",
+            "Emissions|CO2|AFOLU",
+            "Emissions|CO2|Energy and Industrial Processes",
+            "Emissions|CO2",
+        ]
 years_of_interest = [2030, 2050, 2100]
-save_results = "../Output/"
+save_results = "../Output/LotsOfPlotsOfRelations"
 # if non-null, also plot these quantiles.
-plot_quantiles = [0.0001, 0.05, 0.17, 0.33, 0.5, 0.67, 0.83, 0.95, 0.9999]
+plot_quantiles = None  # [0.05, 0.33, 0.5, 0.67, 0.95]
 # if non-null, save data on the quantiles too
-quantiles_savename = "../output-examples"
+quantiles_savename = None
 # How many boxes are used to fit the quantiles?
-quantile_boxes = 10
+quantile_boxes = 20
 # Should we extend the quantile boxes by an additional factor?
-quantile_decay_factor = 0.7
+quantile_decay_factor = 1
 # Color different models different colours?
 model_colours = True
-# In the model-coloured version, how much does the figure need to be reduced by to leave room for the legend?
+# In the model-coloured version, how much does the figure need to be reduced by to leave
+# room for the legend?
 legend_fraction = 0.65
 # ________________________________________________________
 # where do we get the data from?
-SR15_SCENARIOS = "./sr15_scenarios.csv"
+SR15_SCENARIOS = "./sr15_scenarios_more_regions.csv"
 sr15_data = download_or_load_sr15(SR15_SCENARIOS)
 
-silicone.plotting._plot_emission_correlations_quantile_rolling_windows(
-    sr15_data,
-    save_results,
-    years_of_interest,
-    plot_quantiles,
-    quantile_boxes,
-    quantile_decay_factor,
-    model_colours,
-    legend_fraction,
-)
+for ind in range(len(x_gases)):
+    x_gas = x_gases[ind]
+    silicone.plotting._plot_emission_correlations_quantile_rolling_windows(
+        sr15_data,
+        save_results,
+        years_of_interest,
+        x_gas,
+        plot_quantiles,
+        quantile_boxes,
+        quantile_decay_factor,
+        model_colours,
+        legend_fraction,
+    )
--- src/silicone/stats.py
+++ src/silicone/stats.py
@@ -3,6 +3,7 @@ Silicone's custom statistical operations.
 """
 import numpy as np
 import pandas as pd
+import os
 
 
 def rolling_window_find_quantiles(
@@ -75,3 +76,137 @@ def rolling_window_find_quantiles(
                 ys[cumsum_weights >= quantiles[i_quantile]]
             )
     return quantmatrix
+
+
+def calc_all_emissions_correlations(emms_df, years, output_dir):
+    """
+   Saves csv files of the correlation coefficients and the rank correlation
+    coefficients between emissions at specified locations. This function includes all
+    undivided emissions (i.e. results recorded as `Emissions|X`) and CO2 emissions
+    split once (i.e. `Emissions|CO2|X`). It does not include Kyoto gases. It will also
+    save the average absolute value of the coefficients.
+    Files created:
+    "variable_counts.csv" :
+        the number of scenario/model pairs where the emissions data occurs.
+    "gases_correlation_{year}.csv" :
+        The Pearson's correlation between gases emissions in a given year.
+    "gases_rank_correlation_{year}.csv" :
+        The Spearman's rank correlation between gases in a given year
+    "time_av_absolute_correlation_{}_to_{}.csv" :
+        The magnitude of the Pearson's correlation between emissions, averaged over the
+        years requested.
+    "time_av_absolute_rank_correlation_{}_to_{}.csv" :
+        The magnitude of the Spearman's rank correlation between emissions, averaged
+        over the years requested.
+
+    Parameters
+    ----------
+    emms_df : :obj:`pyam.IamDataFrame`
+        The database to search for correlations between named values
+
+    output_dir : str
+        The folder location to save the files.
+
+    years : list[int]
+        The years upon which to calculate correlations.
+    """
+    assert len(emms_df.regions()) == 1, "Calculation is for only one region"
+    # Obtain the list of gases to examine
+    df_gases = (
+        emms_df.filter(level=1)
+            .filter(variable="Emissions|*")
+            .filter(variable="Emissions|Kyoto*", keep=False)
+            .append(
+            emms_df.filter(level=2)
+                .filter(variable="Emissions|CO2*")
+        ).variables(True).set_index("variable")
+    )
+    all_correlations_df = pd.DataFrame(
+        index=df_gases.index, columns=df_gases.index, data=0
+    )
+    all_rank_corr_df = pd.DataFrame(
+        index=df_gases.index, columns=df_gases.index, data=0
+    )
+
+    # Calculate the total amount of data
+    var_count_file = "variable_counts.csv"
+    var_count = pd.Series(index=df_gases.index, dtype=int)
+    for var in df_gases.index:
+        var_db = emms_df.filter(variable=var)
+        var_count[var] = len(var_db.timeseries())
+    var_save_loc = os.path.join(output_dir, var_count_file)
+    var_count.to_csv(var_save_loc)
+    print("Counted the number of each variable and saved to ".format(var_save_loc))
+
+    for year_of_interest in years:
+        # Initialise the tables to hold all parameters between runs
+        correlations_df = pd.DataFrame(index=df_gases.index, columns=df_gases.index)
+        rank_corr_df = pd.DataFrame(index=df_gases.index, columns=df_gases.index)
+        # Check that the list has only one entry for each gas
+        assert not any(df_gases.index.duplicated()), "Index contains duplicated entries"
+        formatted_df = emms_df.filter(
+            variable=df_gases.index, year=year_of_interest
+        ).pivot_table(
+            ["year", "model", "scenario", "region"],
+            ["variable"],
+            aggfunc="mean"
+        )
+        formatted_df.replace(r"^\s*$", np.nan, regex=True, inplace=True)
+        for x_gas_ind in range(df_gases.count()[0]):
+            x_gas = df_gases.index[x_gas_ind]
+            for y_gas_ind in range(x_gas_ind+1, df_gases.count()[0]):
+                y_gas = df_gases.index[y_gas_ind]
+                # Calculate the correlations. This requires removing NAs
+                correlations_df.at[y_gas, x_gas] = formatted_df.corr("pearson").loc[
+                    x_gas, y_gas
+                ]
+                rank_corr_df.at[y_gas, x_gas] = formatted_df.corr("spearman").loc[
+                    x_gas, y_gas
+                ]
+                all_correlations_df.loc[y_gas, x_gas] = all_correlations_df.at[
+                                                           y_gas, x_gas
+                    ] + abs(correlations_df.loc[y_gas, x_gas]) / len(years)
+                all_rank_corr_df.loc[y_gas, x_gas] = all_rank_corr_df.at[
+                                                        y_gas, x_gas
+                    ] + abs(rank_corr_df.at[y_gas, x_gas]) / len(years)
+                # the other parts follow by symmetry
+                correlations_df.at[x_gas, y_gas] = correlations_df.at[y_gas, x_gas]
+                rank_corr_df.at[x_gas, y_gas] = rank_corr_df.at[y_gas, x_gas]
+                all_correlations_df.loc[x_gas, y_gas] = all_correlations_df.at[
+                    y_gas, x_gas
+                ]
+                all_rank_corr_df.loc[x_gas, y_gas] = all_rank_corr_df.at[y_gas, x_gas]
+            print("Finished x_gas {} in year {}.".format(x_gas, year_of_interest))
+        if output_dir is not None:
+            correlations_df.to_csv(
+                os.path.join(
+                    output_dir, "gases_correlation_{}.csv".format(
+                        year_of_interest
+                    )
+                )
+            )
+            rank_corr_df.to_csv(
+                os.path.join(
+                    output_dir, "gases_rank_correlation_{}.csv".format(
+                        year_of_interest
+                    )
+                )
+            )
+    for gas in df_gases.index:
+        all_correlations_df.loc[gas, gas] = np.nan
+        all_rank_corr_df.loc[gas, gas] = np.nan
+    if output_dir is not None:
+        all_correlations_df.to_csv(
+            os.path.join(
+                output_dir, "time_av_absolute_correlation_{}_to_{}.csv".format(
+                    min(years), max(years)
+                )
+            )
+        )
+        all_rank_corr_df.to_csv(
+            os.path.join(
+                output_dir, "time_av_absolute_rank_correlation_{}_to_{}.csv".format(
+                    min(years), max(years)
+                )
+            )
+        )
