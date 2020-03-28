5b0ba38b00790a72b247077cf0dc8a40106be2d9
Lamboll <rlamboll@ic.ac.uk>
2020-03-28 15:45:38 +0000

    Added a capacity to give factors to the composite values infiller

--- src/silicone/multiple_infillers/infill_composite_values.py
+++ src/silicone/multiple_infillers/infill_composite_values.py
@@ -4,10 +4,11 @@ from silicone.utils import _construct_consistent_values
 logger = logging.getLogger(__name__)
 
 
-def infill_composite_values(df, composite_dic=None):
+def infill_composite_values(df, composite_dic=None, factors_dic=None):
     """
     Constructs a series of aggregate variables, calculated as the sums of variables
-    that have been reported.
+    that have been reported. If given factors terms too, the terms will be multiplied
+    by the factors before summing.
 
     Parameters
     ----------
@@ -15,10 +16,11 @@ def infill_composite_values(df, composite_dic=None):
         Input data from which to construct consistent values. This is assumed to be
         fully infilled. This will not be checked.
 
-    composite_dic : dict {str: list[str]}
+    composite_dic : dict {str: list[str] or dict{str: float}}
         Key: The variable names of the composite. Value: The variable names of the
-        constituents, which may include wildcards ('*'). Defaults to a list of
-        PFC, HFC, F-Gases, CO2 and Kyoto gases.
+        constituents, which may include wildcards ('*'). Optionally, these may be
+        dictionaries of the names to factors, which we multiply the numbers by before
+         summing them. Defaults to a list of PFC, HFC, F-Gases, CO2 and Kyoto gases.
     """
     if not composite_dic:
         composite_dic = {
@@ -54,11 +56,25 @@ def infill_composite_values(df, composite_dic=None):
             to_delete.append(composite)
     logging.getLogger("pyam.core").setLevel(logging.WARNING)
     [composite_dic.pop(x) for x in to_delete]
+    # If the composite variables already exist, remove them first
     df = df.filter(variable=composite_dic.keys(), keep=False)
     for composite in composite_dic.keys():
-        composite_df = _construct_consistent_values(
-            composite, composite_dic.get(composite), df
-        )
+        if type(composite_dic.get(composite)) is dict:
+            temp_df = df.filter(
+                variable=[composite] + list(composite_dic.get(composite).keys())
+            )
+            for factor in composite_dic.get(composite).keys():
+                temp_df.data["value"].loc[temp_df.data["variable"] == factor] = \
+                temp_df.data["value"].loc[
+                    temp_df.data["variable"] == factor
+                    ] * composite_dic.get(composite).get(factor)
+            composite_df = _construct_consistent_values(
+                composite, composite_dic.get(composite), temp_df
+            )
+        else:
+            composite_df = _construct_consistent_values(
+                composite, composite_dic.get(composite), df
+            )
         try:
             to_return.append(composite_df)
         except NameError:
--- tests/integration/multiple_infillers/test_infill_composite_values.py
+++ tests/integration/multiple_infillers/test_infill_composite_values.py
@@ -2,6 +2,7 @@ from silicone.multiple_infillers.infill_composite_values import infill_composite
 from silicone.utils import convert_units_to_MtCO2_equiv
 import numpy as np
 import pandas as pd
+import pytest
 
 
 class TestInfillCompositeValues:
@@ -94,3 +95,46 @@ class TestInfillCompositeValues:
             4
             + 2 * 28,  # The 2*28 comes from the CH4, converted to CO2 equiv using AR5.
         )
+
+    def test_infill_with_factors(self, larger_df):
+        # Ensure that the multiplier works correctly
+        larger_df_copy = larger_df.copy()
+        half_industry_df = infill_composite_values(
+            larger_df_copy,
+            composite_dic={
+                "Emissions|CO2": {
+                    "Emissions|CO2|AFOLU": 1, "Emissions|CO2|Industry": 0.5
+                }
+            }
+        )
+        assert np.allclose(
+            half_industry_df.filter(variable="Emissions|CO2")["value"].values,
+            [2, 2, 2, 1, 1, 1, 2, 2, 2]
+        )
+
+    def test_infill_composite_values_subtraction(self, larger_df, caplog):
+        # Ensure that the code performs correctly when we subtract emissions too
+        larger_df_copy = larger_df.copy()
+        larger_df_copy.append(
+            infill_composite_values(
+                larger_df_copy,
+                composite_dic={"Emissions|CO2": ["Emissions|CO2|*"]}
+            ),
+            inplace=True
+        )
+        AFOLU = "Emissions|CO2|AFOLU"
+        larger_df_copy = convert_units_to_MtCO2_equiv(larger_df_copy)
+        forgot_AFOLU = larger_df_copy.filter(variable=AFOLU, keep=False)
+        infilled = infill_composite_values(
+            forgot_AFOLU,
+            composite_dic={AFOLU: {
+                    "Emissions|CO2": 1, "Emissions|CO2|Industry": -1
+                }
+            }
+        )
+        # We should have reconstructed the original data where it existed and also have
+        # 0s now
+        assert infilled.filter(model="model_C").data.equals(
+            larger_df_copy.filter(variable=AFOLU).data.reset_index(drop=True)
+        )
+        assert np.allclose(infilled.filter(model="model_C", keep=False)["value"], 0)
