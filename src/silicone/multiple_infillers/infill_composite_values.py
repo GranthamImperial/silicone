cd2840e0b4e76c5ae4522c0fa737019797976dd5
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-03-29 21:54:26 +1100

    Tidy up

--- src/silicone/multiple_infillers/infill_composite_values.py
+++ src/silicone/multiple_infillers/infill_composite_values.py
@@ -20,7 +20,12 @@ def infill_composite_values(df, composite_dic=None):
         Key: The variable names of the composite. Value: The variable names of the
         constituents, which may include wildcards ('*'). Optionally, these may be
         dictionaries of the names to factors, which we multiply the numbers by before
-         summing them. Defaults to a list of PFC, HFC, F-Gases, CO2 and Kyoto gases.
+        summing them. Defaults to a list of PFC, HFC, F-Gases, CO2 and Kyoto gases.
+
+    Returns
+    -------
+    :obj:`pyam.IamDataFrame`
+        :obj:`pyam.IamDataFrame` containing the composite values.
     """
     if not composite_dic:
         composite_dic = {
@@ -41,39 +46,45 @@ def infill_composite_values(df, composite_dic=None):
                 "Emissions|F-Gases",
             ],
         }
+
     # Do a simple test to ensure we have any relevant data. This does not prevent
     # problems from missing subsets of data.
     to_delete = []
+
     # Disable core logging to prevent warnings about empty filters
     logging.getLogger("pyam.core").setLevel(logging.CRITICAL)
-    for composite in composite_dic.keys():
-        if df.filter(variable=composite_dic.get(composite)).data.empty:
-            logger.warning("No data found for {}".format(composite_dic.get(composite)))
+    for composite, composite_variables in composite_dic.items():
+        if df.filter(variable=composite_variables).data.empty:
+            logger.warning("No data found for {}".format(composite_variables))
             to_delete.append(composite)
+
     logging.getLogger("pyam.core").setLevel(logging.WARNING)
-    [composite_dic.pop(x) for x in to_delete]
+    for x in to_delete:
+        composite_dic.pop(x)
+
     # If the composite variables already exist, remove them first
     df = df.filter(variable=composite_dic.keys(), keep=False)
-    for composite in composite_dic.keys():
-        if type(composite_dic.get(composite)) is dict:
+    for composite, composite_variables in composite_dic.items():
+        if isinstance(composite_variables, dict):
             temp_df = df.filter(
-                variable=[composite] + list(composite_dic.get(composite).keys())
+                variable=[composite] + list(composite_variables.keys())
             )
-            for factor in composite_dic.get(composite).keys():
+            for contributor, factor in composite_variables.items():
                 temp_df.data["value"].loc[
-                    temp_df.data["variable"] == factor
-                ] = temp_df.data["value"].loc[
-                    temp_df.data["variable"] == factor
-                ] * composite_dic.get(composite).get(factor)
+                    temp_df.data["variable"] == contributor
+                ] *= factor
+
             composite_df = _construct_consistent_values(
-                composite, composite_dic.get(composite), temp_df
+                composite, composite_variables, temp_df
             )
+
         else:
             composite_df = _construct_consistent_values(
-                composite, composite_dic.get(composite), df
+                composite, composite_variables, df
             )
         try:
             to_return.append(composite_df)
         except NameError:
             to_return = composite_df
+
     return to_return
