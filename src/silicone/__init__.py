54a64d7946c68346c7f96bac19c4c8406a5625c4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-05-11 09:18:17 +1000

    Try different strategy

--- src/silicone/__init__.py
+++ src/silicone/__init__.py
@@ -1,7 +1,4 @@
 import logging
-from sys import platform
-
-import matplotlib as mpl
 
 from ._version import get_versions
 
@@ -9,8 +6,3 @@ __version__ = get_versions()["version"]
 del get_versions
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-# work around matplotlib explosions, thanks
-# https://github.com/uber/ludwig/commit/6b948ea9f0b2e78558fb51d2edd4d9c3558ff505
-if platform == "darwin":  # OS X
-    mpl.use("TkAgg")
