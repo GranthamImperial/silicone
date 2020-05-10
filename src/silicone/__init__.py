07a4eb353795768c216a5d6c9fa38d4431cbe975
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-05-11 09:01:09 +1000

    Work around matplotlib explosions

--- src/silicone/__init__.py
+++ src/silicone/__init__.py
@@ -1,4 +1,7 @@
 import logging
+from sys import platform
+
+import matplotlib as mpl
 
 from ._version import get_versions
 
@@ -6,3 +9,8 @@ __version__ = get_versions()["version"]
 del get_versions
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# work around matplotlib explosions, thanks
+# https://github.com/uber/ludwig/commit/6b948ea9f0b2e78558fb51d2edd4d9c3558ff505
+if platform == "darwin":  # OS X
+    mpl.use("TkAgg")
