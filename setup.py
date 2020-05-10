5f85f9a4dd0338f2761a966ea7642170dab372ca
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-05-11 08:52:24 +1000

    Remove Python 3.8 testing

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ CLASSIFIERS = [
     "License :: OSI Approved :: BSD License",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    # "Programming Language :: Python :: 3.8",
 ]
 
 REQUIREMENTS_INSTALL = [
